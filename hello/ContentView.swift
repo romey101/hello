50a15d0d50e86066f4c3586b978d44d499e6036c
tawq <noturavgtech@icloud.com>
2024-09-26 17:03:28 +0300

    Initial Commit

---
+++ hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by maram on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by maram on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
